58eaa85fa0d33dae51f098be14826040355cf370
Aaron Long <aaron.long.c@gmail.com>
2017-05-18 18:24:31 -0400

    Started Adding elements

---
+++ src/main/java/io/github/aaronclong/generics/MyArrayList.java
@@ -0,0 +1,87 @@
+package io.github.aaronclong.generics;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Spliterator;
+import java.util.function.UnaryOperator;
+
+/**
+ * Created by aaronlong on 5/18/17.
+ */
+public class MyArrayList<E> {
+// public boolean add(E e) {
+//    return false;
+//  }
+//
+//  public void add(int index, E element) {}
+//
+//  public boolean addAll(Collection<? extends E> c) {}
+//
+//  public boolean addAll(int index, Collection<? extends E> c) {}
+//
+//  public void clear() {}
+//
+//  public boolean contains(Object o) {
+//    return false;
+//  }
+//
+//  public boolean containsAll(Collection<?> c) {}
+//
+//  public boolean equals(Object o) {}
+//
+//  public E get(int index) {}
+//
+//  public int hashCode() {}
+//
+//  public int indexOf(Object o) {}
+//
+//  public boolean isEmpty() {}
+//
+//  public Iterator<E> iterator() {}
+//
+//  public int lastIndexOf(Object o) {}
+//
+//  public ListIterator<E> listIterator() {}
+//
+//  public ListIterator<E> listIterator(int index) {}
+//
+//  public E remove(int index) {}
+//
+//  public boolean remove(Object o) {
+//    return false;
+//  }
+//
+//  public boolean removeAll(Collection<?> c) {}
+//
+//  public void replaceAll(UnaryOperator<E> operator) {}
+//
+//  public boolean retainAll(Collection<?> c) {}
+//
+//  public E set(int index, E element) {}
+//
+//  public int size() {
+//    return 0;
+//  }
+//
+//  public void sort(Comparator<? super E> c) {
+//    return new Comparator<E>();
+//  }
+//
+//  public Spliterator<E> spliterator() {}
+//
+//  public List<E> subList(int fromIndex, int toIndex) {
+//    return new MyArrayList<E>();
+//  }
+//
+//  public Object[] toArray() {
+//    return new Object[1];
+//  }
+//
+//  public <T> T[] toArray(T[] a) {
+//    return (T) new Object[];
+//  }
+//
+}
---
+++ src/test/java/io/github/aaronclong/generics/MyArrayListTest.java
@@ -0,0 +1,7 @@
+package io.github.aaronclong.generics;
+
+/**
+ * Created by aaronlong on 5/18/17.
+ */
+public class MyArrayListTest {
+}
